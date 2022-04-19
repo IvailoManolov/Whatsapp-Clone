8a4873be018956452db674bd6629f4284b0e2229
IvailoManolov <ivomoreras@abv.bg>
2022-04-19 19:43:36 +0300

    cleaned up and added a firebase config object

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = 
+{
+    apiKey : "AIzaSyD2FUrTc_UK3CZ1XW2lsjjalNc-22Tev_E",
+    authDomain : "whatsapp-clonemain.firebaseapp.com",
+    projectId : "whatsapp-clonemain",
+    storageBucket : "whatsapp-clonemain.appspot.com",
+    messagingSenderId : "260682761366",
+    appId : "1:260682761366:web:a0905513e2737afb2d6eb7"
+};
